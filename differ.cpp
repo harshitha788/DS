1a29f9a9d0831d4031353a937b781c6350e5e90f
harshitha788 <harshithan141@gmail.com>
2026-02-09 21:19:21 +0530

    add initial files

---
+++ differ.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int A,B;
+    cout<<"Enter number A: ";
+    cin>>A;
+    cout<<"Enter number B: ";
+    cin>>B;
+    cout<<"The difference of "<<A<<" and "<<B<<" is "<<A-B<<endl;
+    return 0;
+}
