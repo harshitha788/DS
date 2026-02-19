f455e06fdc60950e9a6d16b049cdeb9023c09521
harshitha788 <harshithan141@gmail.com>
2026-02-19 23:50:41 +0530

    array creation

---
+++ Arrays/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;    
+int main()
+{
+    int marks[5]={10,20,30,40,50};
+    cout<<"marks[0]: "<<marks[0]<<endl;
+    cout<<"marks[1]: "<<marks[1]<<endl;
+    cout<<"marks[2]: "<<marks[2]<<endl;
+    cout<<"marks[3]: "<<marks[3]<<endl;
+    cout<<"marks[4]: "<<marks[4]<<endl;
+    return 0;
+}
