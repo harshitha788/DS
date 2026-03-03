151e258db7a15382e74cfdc7c791fb0557d26c49
harshitha788 <harshithan141@gmail.com>
2026-03-03 22:51:19 +0530

    commit

--- Patterns/star_diamond.cpp
+++ Patterns/star_diamond.cpp
@@ -6,10 +6,10 @@ int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         //diamond
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<n-i-1;j++){
             cout<<"$";
         }
-        for(int k=0;k<i;k++){
+        for(int k=0;k<i+1;k++){
             cout<<"*";
         }
         cout<<endl;
